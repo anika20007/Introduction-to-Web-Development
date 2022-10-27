464ae4ba5addfbea253927fda4f5970a9782fac0
anika20007 <57230064+anika20007@users.noreply.github.com>
2022-10-27 10:38:36 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Course Policies\n");
+    printf("3.mid-term and final examintaions will be held on scheduled time.you must take\npermission from department for make up examinations.\n");
+    printf("1.all assingments will be subject to strict deadline.late submission will not be\naccepted without prior permission.\n");
+    printf("2.two short quizes will be held on introduction to computer .2-4 in class coding\ntests will be held.\n");
+    printf("4.performance in the final viva will also affect marks of the corresponding\nassingments ,tests or examintions\n");
+    printf("he said,\"leave me alone.\"\n\n\n\n\n");
+    //variables//
+    int num;//declaring a variable//
+    num = 658889765895689765879658977856986565687;//assigning a value to num//
+    printf("The number is %d",num);
+    int i = 76889566658944354565;
+    float f = 8568856896.8658658;
+    char c;
+    c = '$';
+
+    printf("the integer is %d\n",i);
+    printf("the frac is %f\n",f);
+    printf("the charc is %c\n,",c);
+
+    int t = 8895;
+    int u = 879986;
+    float q = 85869.567;
+    float r = 87564.87;
+    printf("integer is %d\n",t);
+     printf ("integer is %d\n",u);
+ printf("integer is %f\n",q);
+  printf("integer is %f\n",r);
+    return 0;
+}
